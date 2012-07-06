721c90cfbe4984f3bf423e6b29b7efdee1db4896
belingueres <belingueres@9d92ac12-a794-442d-8c22-faf8e28c1b4f>
2012-07-06 22:43:56 +0000

    Agregado el archivo de test/java/com/hexaid/struts2/interceptor/TestConversationInterceptorPerViewConversationExpirationPolicyIntegration.java...parece que no estaba agregado al svn...raro...

---
+++ trunk/proyectos/struts2-conversations/src/test/java/com/hexaid/struts2/interceptor/TestConversationInterceptorPerViewConversationExpirationPolicyIntegration.java
@@ -0,0 +1,377 @@
+package com.hexaid.struts2.interceptor;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.Map;
+
+import javax.servlet.ServletContextEvent;
+
+import org.apache.struts2.tiles.StrutsTilesListener;
+import org.junit.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import com.hexaid.struts2.conversations.Conversation;
+import com.hexaid.struts2.junit.Config;
+import com.hexaid.struts2.junit.StrutsBaseTestCase;
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * @author Gabriel Belingueres
+ *
+ */
+@Config(file={"struts-plugin.xml", "struts-perviewexpiration.xml"})
+public class TestConversationInterceptorPerViewConversationExpirationPolicyIntegration
+		extends StrutsBaseTestCase {
+	
+	/**
+	 * Test that the conversation timeout is modified in the Dispatch result type
+	 */
+	@Test
+	@Config(actionName="beginConvWithExpiration")
+	public void testConversationTimeoutWhenModifiedInDispatchResult() {
+		try {
+			String result = actionProxy.execute();
+			assertEquals(ActionSupport.SUCCESS, result);
+
+			final String conversationId = "1";
+			Conversation conversation = getAllConversationsMap().get(conversationId);
+			
+			assertNotNull(conversation);
+			// modified in the result part inside the xml file
+			assertEquals(20, conversation.getMaxInactiveInterval());
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("The action execution failed");
+		}
+	}	
+
+	/**
+	 * Test that the conversation timeout is modified in the Redirect result type
+	 * TODO: Resolver el problema de este TEST!
+	 */
+//	@Test
+//	@Config(actionName="beginConvWithExpirationRedirect")
+//	public void testConversationTimeoutWhenModifiedInRedirectResult() {
+//		try {
+//			String result = actionProxy.execute();
+//			assertEquals(ActionSupport.SUCCESS, result);
+//
+//			final String conversationId = "1";
+//			Conversation conversation = getAllConversationsMap().get(conversationId);
+//			
+//			assertNotNull(conversation);
+//			// modified in the result part inside the xml file
+//			assertEquals(20, conversation.getMaxInactiveInterval());
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//			fail("The action execution failed");
+//		}
+//	}	
+
+	/**
+	 * Test that the conversation timeout is modified in the RedirectAction result type
+	 * TODO: Resolver el problema de este TEST!
+	 */
+//	@Test
+//	@Config(actionName="beginConvWithExpirationRedirectAction")
+//	public void testConversationTimeoutWhenModifiedInRedirectActionResult() {
+//		try {
+//			String result = actionProxy.execute();
+//			assertEquals(ActionSupport.SUCCESS, result);
+//
+//			final String conversationId = "1";
+//			Conversation conversation = getAllConversationsMap().get(conversationId);
+//			
+//			assertNotNull(conversation);
+//			// modified in the result part inside the xml file
+//			assertEquals(20, conversation.getMaxInactiveInterval());
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//			fail("The action execution failed");
+//		}
+//	}	
+
+	/**
+	 * Test that the conversation timeout is modified in the Dispatch result type
+	 */
+	@Test
+	@Config(actionName="beginConvWithExpirationTiles")
+	public void testConversationTimeoutWhenModifiedInTilesResult() {
+		try {
+			// set up Tiles
+			servletContext.addInitParameter("org.apache.tiles.impl.BasicTilesContainer.DEFINITIONS_CONFIG", "tiles.xml");
+			StrutsTilesListener listener = new StrutsTilesListener();
+			ServletContextEvent event = new ServletContextEvent(servletContext);
+			listener.contextInitialized(event);
+			
+			String result = actionProxy.execute();
+			assertEquals(ActionSupport.SUCCESS, result);
+
+			final String conversationId = "1";
+			Conversation conversation = getAllConversationsMap().get(conversationId);
+			
+			assertNotNull(conversation);
+			// modified in the result part inside the xml file
+			assertEquals(20, conversation.getMaxInactiveInterval());
+			
+			// teardown Tiles
+			listener.contextDestroyed(event);
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("The action execution failed");
+		}
+	}
+
+	/**
+	 * Test access an expired conversation
+	 */
+	@Test
+	@Config(actionName="beginConvWithExpiration")
+	public void testConversationLastAccessedTimeIsUpdated() {
+		try {
+			String result = actionProxy.execute();
+			assertEquals(ActionSupport.SUCCESS, result);
+
+			final String conversationId = "1";
+			Conversation conversation = getAllConversationsMap().get(conversationId);
+			
+			assertNotNull(conversation);
+			assertTrue(conversation.getLastAccessTime() > 0);
+			assertFalse(conversation.isEnded());
+			
+			long lastAccessTimeFirstRequest = conversation.getLastAccessTime();
+			
+			// wait some time
+			Thread.sleep(100);
+			
+			// second request
+			request = new MockHttpServletRequest();
+			request.setSession(session);
+			request.addParameter(Conversation.CONVERSATION_ID_PARAM, conversationId);
+			
+			response = new MockHttpServletResponse();
+			createActionProxy(null, "mandatoryAction", null);
+			
+			String result2 = actionProxy.execute();
+			assertEquals(ActionSupport.SUCCESS, result2);
+			
+			// should not found
+			Conversation conversation2 = getAllConversationsMap().get(conversationId);
+			assertNotNull(conversation2);
+
+			long lastAccessTimeSecondRequest = conversation2.getLastAccessTime();
+			
+			assertTrue(lastAccessTimeSecondRequest > lastAccessTimeFirstRequest);
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("The action execution failed");
+		}
+	}
+
+	/**
+	 * Test access to a Foreground conversation when it should be expired if it were a 
+	 * background one.
+	 */
+	@Test
+	@Config(actionName="beginConvWithExpiration")
+	public void testForegroundConversationNotExpired() {
+		try {
+			String result = actionProxy.execute();
+			assertEquals(ActionSupport.SUCCESS, result);
+
+			final String conversationId = "1";
+			Conversation conversation = getAllConversationsMap().get(conversationId);
+			
+			assertNotNull(conversation);
+			assertFalse(conversation.isEnded());
+			
+			// now FORCE the conversation to expire, by setting the lastAccessTime to 2 
+			// minutes before
+			conversation.setLastAccessTime(conversation.getLastAccessTime() - (120*1000));
+			
+			// second request
+			request = new MockHttpServletRequest();
+			request.setSession(session);
+
+			// make it the foreground conversation
+			request.addParameter(Conversation.CONVERSATION_ID_PARAM, conversationId);
+			
+			response = new MockHttpServletResponse();
+			createActionProxy(null, "mandatoryAction", null);
+			
+			String result2 = actionProxy.execute();
+			assertEquals(ActionSupport.SUCCESS, result2);
+			
+			// should be found
+			Conversation conversation2 = getAllConversationsMap().get(conversationId);
+			assertNotNull(conversation2);
+			assertFalse(conversation2.isEnded());
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("The action execution failed");
+		}
+	}
+
+	/**
+	 * Test access to a Foreground conversation when it should be expired if it were a 
+	 * background one, and test that expired background conversation is removed.
+	 */
+	@Test
+	@Config(actionName="beginConvWithExpiration")
+	public void testForegroundConversationNotExpiredButBackgroundExpired() {
+		try {
+			// first request, creating conversation id=1
+			String result = actionProxy.execute();
+			assertEquals(ActionSupport.SUCCESS, result);
+
+			final String conversationId = "1";
+			Conversation conversation = getAllConversationsMap().get(conversationId);
+			
+			assertNotNull(conversation);
+			assertFalse(conversation.isEnded());
+			
+			// second request, creating conversation id=2
+			request = new MockHttpServletRequest();
+			request.setSession(session);
+
+			createActionProxy(null, "beginConvWithExpiration", null);
+			String result2 = actionProxy.execute();
+			assertEquals(ActionSupport.SUCCESS, result2);
+
+			final String conversationId2 = "2";
+			Conversation conversation2 = getAllConversationsMap().get(conversationId2);
+			
+			assertNotNull(conversation2);
+			assertFalse(conversation2.isEnded());
+			
+			// now FORCE NOTH conversations to expire, by setting the lastAccessTime to 2 
+			// minutes before
+			conversation.setLastAccessTime(conversation.getLastAccessTime() - (120*1000));
+			conversation2.setLastAccessTime(conversation.getLastAccessTime() - (120*1000));
+			
+			// third request
+			request = new MockHttpServletRequest();
+			request.setSession(session);
+
+			// make it the foreground conversation
+			request.addParameter(Conversation.CONVERSATION_ID_PARAM, conversationId);
+			
+			response = new MockHttpServletResponse();
+			createActionProxy(null, "mandatoryAction", null);
+			
+			String result3 = actionProxy.execute();
+			assertEquals(ActionSupport.SUCCESS, result3);
+
+			// only the foreground should remain
+			assertEquals(1, getAllConversationsMap().size());
+			
+			// should be found
+			Conversation conversation3 = getAllConversationsMap().get(conversationId);
+			assertNotNull(conversation3);
+			assertFalse(conversation3.isEnded());
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("The action execution failed");
+		}
+	}
+
+	/**
+	 * Test that if I make a request with a non existent conversation, then the expiration
+	 * of other conversation is NOT run
+	 */
+	@Test
+	@Config(actionName="beginConvWithExpiration")
+	public void testNotExpiredForegroundAfterRequestingAnExpiredConversation() {
+		try {
+			// first request, creating conversation id=1
+			String result = actionProxy.execute();
+			assertEquals(ActionSupport.SUCCESS, result);
+
+			final String conversationId = "1";
+			Conversation conversation = getAllConversationsMap().get(conversationId);
+			
+			assertNotNull(conversation);
+			assertFalse(conversation.isEnded());
+			
+			// second request, creating conversation id=2
+			request = new MockHttpServletRequest();
+			request.setSession(session);
+
+			createActionProxy(null, "beginConvWithExpiration", null);
+			String result2 = actionProxy.execute();
+			assertEquals(ActionSupport.SUCCESS, result2);
+
+			final String conversationId2 = "2";
+			Conversation conversation2 = getAllConversationsMap().get(conversationId2);
+			
+			assertNotNull(conversation2);
+			assertFalse(conversation2.isEnded());
+			
+			// now FORCE BOTH conversations to expire, by setting the lastAccessTime to 2 
+			// minutes before
+			conversation.setLastAccessTime(conversation.getLastAccessTime() - (120*1000));
+			conversation2.setLastAccessTime(conversation2.getLastAccessTime() - (120*1000));
+			
+			// third request
+			request = new MockHttpServletRequest();
+			request.setSession(session);
+
+			// make it the foreground conversation
+			request.addParameter(Conversation.CONVERSATION_ID_PARAM, conversationId);
+			
+			response = new MockHttpServletResponse();
+			createActionProxy(null, "mandatoryAction", null);
+			
+			String result3 = actionProxy.execute();
+			assertEquals(ActionSupport.SUCCESS, result3);
+
+			// only the foreground should remain
+			assertEquals(1, getAllConversationsMap().size());
+			
+			// should be found
+			Conversation conversation3 = getAllConversationsMap().get(conversationId);
+			assertNotNull(conversation3);
+			assertFalse(conversation3.isEnded());
+			
+			// fourth request
+
+			// now FORCE valid conversation to expire, by setting the lastAccessTime to 2 
+			// minutes before
+			conversation.setLastAccessTime(conversation.getLastAccessTime() - (120*1000));
+
+			request = new MockHttpServletRequest();
+			request.setSession(session);
+			
+			// make the expired conversation the foreground conversation
+			request.addParameter(Conversation.CONVERSATION_ID_PARAM, conversationId2);
+
+			response = new MockHttpServletResponse();
+			createActionProxy(null, "mandatoryAction", null);
+
+			String result4 = actionProxy.execute();
+			assertEquals(ConversationInterceptor.DEFAULT_CONVERSATION_NOT_FOUND_RESULT, result4);
+
+			// the other conversation should remain, since the requested foreground 
+			// conversation did not exist
+			assertEquals(1, getAllConversationsMap().size());
+			
+			Conversation conversation4 = getAllConversationsMap().get(conversationId);
+			assertNotNull(conversation4);
+			assertFalse(conversation4.isEnded());
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("The action execution failed");
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	private Map<String, Conversation> getAllConversationsMap() {
+		return (Map<String, Conversation>) session.getAttribute(Conversation.CONVERSATIONS_MAP_KEY);
+	}
+
+}
